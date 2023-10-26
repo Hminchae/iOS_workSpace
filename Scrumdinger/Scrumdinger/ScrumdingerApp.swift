c3ae90331886e8ad059c29f4ed397fa405caaaf1
Hminchae <hminchae@gmail.com>
2023-10-26 18:08:57 +0900

    SwiftUI Tutorials - ScrumDinger
    create a card view

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 황민채 on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
---
+++ Scrumdinger/Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,22 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by 황민채 on 10/26/23.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+                   configuration.title
+                   configuration.icon
+               }
+    }
+}
+
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
